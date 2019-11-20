86994f5a9a5d5e8edbdf85654dd87d7dbd73125f
Archan Ray <archanray@wpa079.wpanet.cs.local>
2019-11-19 21:16:46 -0500

    modular main

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from glob import glob
 from tqdm import tqdm
 from utils import fvecs_read, ivecs_read
 from indexed_structure import compute_online
+import argparse
 
 def read_data(full_data_path):
 	"""
@@ -29,12 +30,25 @@ def read_data(full_data_path):
 	return base_vectors, query_vectors
 
 
-path_to_data = "./data/"
-data = "siftsmall"
-full_data_path = os.path.join(path_to_data, data)
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--path_to_data", default="./data", help="path where all data is stored", type=str)
+    parser.add_argument("--dataset", metavar="NAME", help="the dataset to load training points from", default="siftsmall", type=str, choices=["siftsmall"])
+    parser.add_argument("-k", "--count", default=1, type=positive_int, help="the number of near neighbours to search for")
+    parser.add_argument("--method", default="AK", type=str, choices=["AK", "ORS"]
+    args = parser.parse_args()
 
-base_vectors, query_vectors = read_data(full_data_path)
+    path_to_data = args.path_to_data
+    data = args.dataset
+    full_data_path = os.path.join(path_to_data, data)
 
-# compute indexed structure
-method_choices = ["AK", "ORS"]
-selected_vectors = compute_online(base_vectors, method=method_choices[0])
+    base_vectors, query_vectors = read_data(full_data_path)
+
+    # compute indexed structure
+    method_choice = args.method
+    selected_vectors = compute_online(base_vectors, method=method_choice)
+
+    return None
+
+if name == "__main__":
+    main()
